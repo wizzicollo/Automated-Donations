7f05dd7f767de494664b4cc0897058eef169a2c6
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-18 12:31:45 +0300

    made a list of needed api urls

--- Backend/Dona/models.py
+++ Backend/Dona/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Charity(models.Model):
--- Backend/Dona/urls.py
+++ Backend/Dona/urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url 
+from Dona import views 
 #..............
 
 urlpatterns=[
-#..............
+    url(r'^api/charities$', views.charities_list),
+    url(r'^api/charities/(?P<pk>[0-9]+)$', views.charity_detail),
+    url(r'^api/charities/published$', views.charities_list_published)
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
