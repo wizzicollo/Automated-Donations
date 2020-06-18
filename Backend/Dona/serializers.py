cbc5579db9bfe1e01f809c841499a27dd98b3997
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-18 12:31:05 +0300

    Created the serializer class for the charity model

---
+++ Backend/Dona/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers 
+from Dona.models import Charity
+
+class CharitySerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = Charity
+        fields = ('name',
+        'email',
+        'description',
+        'location',
+        'phone_number',
+        'target_amount',
+        'beneficiary',
+        'donors',
+        'registration_date',)
