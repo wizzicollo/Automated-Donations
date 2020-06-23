67acebba0325e075537ef473fa0c26f526f197c1
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-23 19:23:40 +0300

    made changes to how the apis are displayed to match the rest of the group

--- Backend/Donations/settings.py
+++ Backend/Donations/settings.py
@@ -132,5 +132,9 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8081',
+    'http://localhost:4200',
 )
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- Backend/Donations/urls.py
+++ Backend/Donations/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include 
 from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import include, path
+from rest_framework import routers
+from Dona import views
+
+router = routers.DefaultRouter()
+router.register(r'charities',views.CharityViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('Dona.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
